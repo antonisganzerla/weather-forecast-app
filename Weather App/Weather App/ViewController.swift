0f5d185e3ce4fa2c3c8b180a12bdd2e11ba56987
Antoni Sganzerla <antoni.sganzerla@gmail.com>
2024-02-20 11:08:30 -0300

    setup view code

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //private let customView = UIView(frame: .zero)
+    private lazy var customView: UIView = {
+        let view = UIView(frame: .zero)
+        //view.backgroundColor = UIColor.blue
+        view.backgroundColor = .blue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
-
+    
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        view.addSubview(customView)
+    }
+    
+    private func setConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -100)
+        ])
+    }
 
 }
 
